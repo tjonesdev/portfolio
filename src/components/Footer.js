f8d0819da8cad016dd18c41e62695c0a3aa4a17f
tjonesdev <tracey@tjonesdev.com>
2022-02-20 23:28:46 -0600

    feat: make year the current year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
-import React from "react";
-export const Footer = props => {
-  return (
-    <footer>
-      <span className='copyright' aria-label='Copyright'>
-        &copy; 2020 | Tracey Jones &#10084;
-      </span>
-    </footer>
-  );
+import React from 'react';
+export const Footer = (props) => {
+	let date = new Date();
+	let year = date.getFullYear();
+	return (
+		<footer>
+			<span className='copyright' aria-label='Copyright'>
+				&copy; {year} | Tracey Jones &#10084;
+			</span>
+		</footer>
+	);
 };
