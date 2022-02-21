274185965348482d0bb723dface8f90abc135671
tjonesdev <tracey@tjonesdev.com>
2022-02-20 23:27:09 -0600

    refactor: update "about me" page text

--- src/components/About.js
+++ src/components/About.js
@@ -10,21 +10,15 @@ export const About = props => {
         About Me
       </h3>
       <p>
-        I'm a web developer, born and raised in beautiful St. Louis, MO,
-        passionate about learning and exploring new technologies. I strive to
-        glean something new from each application I create, and I'm always
-        searching for the next project to move forward with.
-      </p>
-      <p>
-        Although I specialize in front-end technologies, I'm excited to get my
-        feet wet with full stack as well! I care enormously about making
-        responsive, accessibility-centric designs that are beautiful and
-        innovative.
-      </p>
-      <p>
-        I am always searching for my next opportunity, so drop me a line if
-        you're interested in collaborating with me!
-      </p>
-    </section>
-  );
+				I'm a software engineer based in Kansas City, MO, passionate about
+				learning and exploring new technologies. I strive to learn something new
+				from each application I create, and I'm always searching for the next
+				project to move forward with.
+			</p>
+			<p>
+				Although I specialize in front-end technologies, I do have experience
+				with PHP, and I am interested to learn additional languages as well! I
+				care a lot about making responsive, accessibility-centric websites and
+				applications that are beautiful and innovative. I prioritize the user
+				experience above all else.
 };
